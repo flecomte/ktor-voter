3c3b1b81d6adc0722c07eafa4ca9958e33d69ddb
Fabrice Lecomte <fabrice.lecomte@non.se.com>
2020-03-25 01:15:36 +0100

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.3.70"
+}
+
+val ktor_version: String by project
+
+group = "org.example"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-auth:$ktor_version")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
