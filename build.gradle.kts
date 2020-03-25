10df74032019e01928f470a6b712553bf5c59699
Fabrice Lecomte <fabrice.lecomte@non.se.com>
2020-03-25 14:33:52 +0100

    config package

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
 plugins {
     kotlin("jvm") version "1.3.70"
+    `maven-publish`
     id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
+    id("fr.coppernic.versioning") version "3.1.2"
 }
 
 val ktor_version: String by project
 
-group = "org.example"
-version = "1.0"
+group = "flecomte"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
@@ -24,5 +27,29 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").allSource)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "ktor-voter"
+            url = uri("https://maven.pkg.github.com/flecomte/ktor-voter")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("ktor-voter") {
+            from(components["java"])
+            artifact(sourcesJar)
+        }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "KtorVoter"
+rootProject.name = "ktor-voter"
